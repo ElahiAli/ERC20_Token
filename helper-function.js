21a6fc2cf9eb134fe77c4357ad5f6ea6bf01e8c7
ElahiAli <ali_elahi00@outlook.com>
2023-04-24 14:42:19 +0330

    adding verify scripts

---
+++ helper-function.js
@@ -0,0 +1,21 @@
+const { run } = require("hardhat");
+
+async function verify(contractAddress, args) {
+  console.log("Verifying....");
+  try {
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArgumets: args,
+    });
+  } catch (error) {
+    if (error.message.toLowerCase().includes("already verified")) {
+      console.log("Already verified.");
+    } else {
+      console.log(error);
+    }
+  }
+}
+
+module.exports = {
+  verify,
+};
